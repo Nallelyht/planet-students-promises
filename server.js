26fc38de5b3d879a0cf04fa292b215bad38bba2c
Nallelyht <nallelyht@gmail.com>
2017-07-12 23:55:51 +0200

    agregando materialize

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use('/data', express.static(path.join(__dirname,'data')));
 app.use('/static', express.static(path.join(__dirname,'assets')));
-
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
