72ee5aad61c1cf65c1dbd49dd8455aa6a62b8d8a
Nallely House <nallelyht@gmail.com>
2017-07-13 15:02:18 -0500

    imprime las tarjetas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,52 @@
+var plantilla = '<div class="row">' +
+		'<div class="col s12 m8 l6">' +
+		'<div class="card">' +
+		'<div class="card-content">' +
+		'<span class="card-title">__nombre__</span>' +
+		'<p>__den__ density</p>' +
+		'<p>Discovered in __disc__ with __telescopio__</p>'+
+		'</div>' +
+		'</div>' +
+		'</div>' +
+		'</div>';
+
+var sequence = Promise.resolve();
+
 function getJSON(url){
-  return new Promise(function(resolve,reject){
-    var ajax = new XMLHttpRequest();  
-    ajax.open("GET", url); 
-    ajax.send(); 
-    ajax.onreadystatechange = function(){ 
-      if(ajax.readyState == 4){ 
-        resolve(JSON.parse(ajax.responseText)); 
-      }
-    }
-  })
+	return new Promise(function(resolve,reject){
+		var ajax = new XMLHttpRequest();  
+		ajax.open("GET", url); 
+		ajax.send(); 
+		ajax.onreadystatechange = function(){ 
+			if(ajax.readyState == 4){ 
+				resolve(JSON.parse(ajax.responseText)); 
+			}
+		}
+	})
 };
 
 getJSON("data/earth-like-results.json") 
-  .then(function(mensaje){return(getJSON(mensaje.results[0]))}) 
-  .then(function(resultado){console.log(resultado.pl_name)});
+	.then(function(response){
+	return Promise.all(
+		response.results.map(getJSON));
+
+}).then(function(resultados){
+	var plantillaFinal = " ";
+
+	resultados.forEach(function(resultado){
+		var nombre = resultado.pl_name;
+		var densidad = resultado.dec;
+		var descubrimiento = resultado.pl_disc;
+		var telescopio = resultado.pl_telescope;  
+
+
+		console.log(nombre,densidad,descubrimiento,telescopio);
+
+		plantillaFinal += plantilla.replace('__nombre__', nombre).replace('__den__', densidad).replace('__disc__', descubrimiento).replace('__telescopio__', telescopio) ;		
+		document.getElementById("cards-container").innerHTML = plantillaFinal;
+	});
+
+})
+
+//  .then(function(mensaje){return(getJSON(mensaje.results[0]))}) 
+//  .then(function(resultado){console.log(resultado.pl_name)});
--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080);
+app.listen(1234);
